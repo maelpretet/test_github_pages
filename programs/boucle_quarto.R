890ce3e56b3b2ac6e2993fc3ca2279e0d553fd42
mael.pretet <mael.pretet1@mnhn.fr>
2025-10-09 17:31:54 +0200

    feat: ajout de la boucle quarto

---
+++ programs/boucle_quarto.R
@@ -0,0 +1,50 @@
+# Librairies
+library(dplyr)
+library(here)
+library(quarto)
+library(stringr)
+
+# Fonctions
+if (Sys.getenv("CI") != "true") {
+  readRenviron(".env")
+}
+
+#liste principale des papillons de l'observatoire
+if (Sys.getenv("CI") == "true") {
+  liste_principale <- c("Amaryllis", "Argus verts", "Belle-dame")
+}else{
+  liste_principale <- c("Amaryllis", "Argus verts", "Belle-dame", "Paon du jour", "Piérides blanches")
+  # liste_principale <- c("Amaryllis", "Argus verts", "Belle-dame", "Citrons",
+  #                       "Paon du jour", "Piérides blanches", "Tabac d'Espagne", "Gazé")
+  # liste_principale <- c("Amaryllis", "Argus verts", "Aurores", "Belle-dame",
+  #                       "Brun des pélargoniums", "Citrons", "Cuivré",
+  #                       "Demi-deuils", "Flambés", "Gazé", "Hespérides orangées",
+  #                       "Hespérides tachetées", "Lycènes bleus", "Machaons",
+  #                       "Mégères", "Moro-sphinx", "Myrtil", "Paon du jour",
+  #                       "Petites tortues", "Piérides blanches", "Procris",
+  #                       "Robert-le-diable", "Silène", "Souci", "Sylvains",
+  #                       "Tabac d'Espagne", "Tircis", "Vulcain")
+}
+
+time = Sys.time()
+# Boucle sur les noms d'espèces
+for (sp_name in liste_principale) {
+
+  tryCatch({
+    filename = paste0("maquette_espece_", sp_name, ".html")
+
+    quarto_render(input = "maquette_espece.qmd",
+                  execute_params = list("sp_name" = sp_name),
+                  output_file = filename)
+
+    file.rename(filename, file.path("out", filename))
+
+  }, error = function(e) {
+    message(sprintf("Error rendering document_%s.qmd: %s", sp_name, e$message))
+  })
+
+}
+  
+print(Sys.time() - time)
+
+
